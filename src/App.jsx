3a740bd5ac0bd82aa60f668aef63bb031b527c5c
Denny7008 <rajgandhadinesh@gmail.com>
2025-04-06 18:36:03 +0530

    some error changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/footer';
+import Navbar from './components/Navbar.jsx';
+import Footer from './components/footer.jsx';
 import MenSectionPage from './pages/Menspage.jsx'; // ← Create this page
 import Home from './pages/Homepage'; // ← Create a Home.jsx for main content
 
